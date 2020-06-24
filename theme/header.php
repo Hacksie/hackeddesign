22f9dea9ce602b75b39010c8fa4b7e777f79e67b
Ben Wise <hackerxse@hotmail.com>
2020-06-24 14:24:57 +1000

    wptheme mk2

---
+++ theme/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="My name is Ben Wise, and I like to style myself as a software development fixer. A software hacker, of sorts (the good kind, mind you), and I'm a specialist in resolving difficult application, enterprise integration and game design problems">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant&display=swap" rel="stylesheet"> 
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="wrapper" class="hfeed">
+        <header id="header">
+            <nav id="menu">
+                <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
+            </nav>
+        </header>
---
+++ theme/index.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<main id="content">
+    <article id="hero" class="hero_display">
+        <div class="hero_column1">
+            <img id="logo" src="assets/images/logo.svg" type="image/svg+xml" />
+        </div>
+        <div class="hero_column2">
+            <ul class="hero_services">
+                <li>author</li>
+                <li>game & script design</li>
+                <li>behavioural application designs</li>
+                <li>legacy system reverse engineering</li>
+                <li>problematic enterprise integrations</li>
+            </ul>
+            <!--
+            <a id="hero_more" class="button" onclick="show_about()">
+                learn more
+            </a>
+            -->
+        </div>
+    </article>
+</main>
+<?php get_footer(); ?>
