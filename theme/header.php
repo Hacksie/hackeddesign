7132c9b6445eb89fb9c5a0060c2e5e36aa236212
Ben Wise <hackerxse@hotmail.com>
2021-06-07 15:21:52 +1000

    wp theme update

--- theme/header.php
+++ theme/header.php
@@ -7,6 +7,8 @@
     <meta name="description" content="My name is Ben Wise, and I like to style myself as a software development fixer. A software hacker, of sorts (the good kind, mind you), and I'm a specialist in resolving difficult application, enterprise integration and game design problems">
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Cormorant&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Tenor+Sans&display=swap" rel="stylesheet"> 
     <?php wp_head(); ?>
 </head>
 
